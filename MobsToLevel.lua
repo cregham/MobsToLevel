a1a62107569cf23a46ec954ff7d8e9dfd0bec3c4
TheOneReed <ariacilon@gmail.com>
2017-01-02 23:57:06 +0900

    Initial commit

---
+++ MobsToLevel.lua
@@ -0,0 +1,77 @@
+----------------------------------------------------------------
+-- EVTCalendar
+-- Author: Reed
+--
+--
+----------------------------------------------------------------
+
+previousMobs = {};
+
+function M2L_OnLoad()
+    this:RegisterEvent("ADDON_LOADED");
+	this:RegisterEvent("PLAYER_XP_UPDATE");
+    this:RegisterEvent("CHAT_MSG_COMBAT_XP_GAIN");
+end
+
+function M2L_OnEvent()
+	if event == "CHAT_MSG_COMBAT_XP_GAIN" then
+		if string.find(arg1, "(.+) dies") then
+			local _, _, killedMob, XPGain = string.find(arg1, "(.+) dies, you gain (%d+) experience.");
+			if GetXPExhaustion() then
+				table.insert(previousMobs, math.floor(XPGain/2));
+			else
+				table.insert(previousMobs, math.floor(XPGain));
+			end
+			if table.getn(previousMobs) > 3 then
+				table.remove(previousMobs, 1)
+			end
+			M2L_Calc(XPGain);
+		end
+	elseif event == "PLAYER_XP_UPDATE" then
+		M2L_Calc();
+	end
+end
+
+function M2L_Calc(XPGain)
+	local restToGo, killsToGo;
+	local avgXP = 0;
+	
+	if not XPGain then
+		XPGain = 0;
+	end
+	
+	local restXP = GetXPExhaustion();
+	local curXP = UnitXP("player") + XPGain;
+	local maxXP = UnitXPMax("player");
+	
+	if restXP then
+		for _,x in pairs(previousMobs) do
+			avgXP = avgXP + x;
+		end
+		avgXP = avgXP / table.getn(previousMobs);
+		if restXP > (maxXP - curXP) then
+			killsToGo = (maxXP - curXP)/(avgXP*2);
+			M2LString:SetText(killsToGo);
+		else
+			restToGo = (restXP / avgXP);
+			killsToGo = (maxXP - curXP - restXP)/(avgXP);
+			killsToGo = math.ceil((killsToGo + restToGo));
+			M2LString:SetText(killsToGo);
+		end
+	else
+		for _,x in pairs(previousMobs) do
+			avgXP = avgXP + x;
+		end
+		avgXP = avgXP / table.getn(previousMobs);
+		killsToGo = math.ceil((maxXP - curXP)/avgXP);
+		M2LString:SetText(killsToGo);
+	end
+end
+
+function M2L_print(str, err)
+	if err == nil then
+		DEFAULT_CHAT_FRAME:AddMessage("|c00FFFF00MobsToLevel: " .. tostring(str) .. "|r");
+	else
+		DEFAULT_CHAT_FRAME:AddMessage("|c00FFFF00MobsToLevel:|r " .. "|c00FF0000Error|r|c006969FF - " .. tostring(str) .. "|r");
+	end
+end
